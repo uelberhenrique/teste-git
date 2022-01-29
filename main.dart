831c5f6dca1c2c8aec690256fa94640fac491345
Uelber Henrique <uelber2011@gmail.com>
2022-01-29 17:33:08 -0300

    Function multiplicacao

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
-void  main() {
-  
+import 'multiplicacao.dart';
+
+void main() {
+  double numberOne = 10;
+  double numberTwo = 20;
+
+  final result = multiplicacao(numberOne, numberTwo);
+
+  print(result);
 }
---
+++ multiplicacao.dart
@@ -0,0 +1,3 @@
+double multiplicacao(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
